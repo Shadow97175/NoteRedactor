d0577b75eaf06c5a5f9ef6dbd7bf73c8e2429d57
Shadow97175 <Shadow_97@mail.ru>
2016-03-23 23:14:25 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //Note note = new Note(1, NoteSound.DO, new NoteLength(0, false));
+
+        NoteDesigner form = new NoteDesigner();
+        form.show();
+
+    }
+}
