e399acf46c4df97886e93522fc5b58a4759d5160
Shadow97175 <Shadow_97@mail.ru>
2016-03-23 23:24:41 +0300

    Create PositionedNote.java

---
+++ PositionedNote.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by gifin_000 on 19.03.2016.
+ */
+public class PositionedNote {
+   public Note note;
+   public int i;
+
+    public PositionedNote(Note note, int i){
+        this.note = note;
+        this.i = i;
+    }
+}
